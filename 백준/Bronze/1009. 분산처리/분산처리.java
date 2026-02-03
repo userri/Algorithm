7d5cfb940d9d05182e944d8d96ab5941f7c80390
Userri <125868433+userri@users.noreply.github.com>
2026-02-03 17:27:42 +0900

    [Bronze II] Title: 분산처리, Time: 124 ms, Memory: 14400 KB -BaekjoonHub

---
+++ 백준/Bronze/1009. 분산처리/분산처리.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+//1 1 1 1
+//        2 4 8 6 2주기 4
+//        3 9 7 1 3주기 4
+//        4 6 4 6주기 2
+//        5
+//        6
+//        7 9 3 1
+//        8 4 2 6
+//        9 1
+
+class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int[][] periods = new int[10][];
+        periods[0] = new int[]{0};
+        periods[1] = new int[]{0};
+        periods[2] = new int[]{2, 4, 8, 6};
+        periods[3] = new int[]{3, 9, 7, 1};
+        periods[4] = new int[]{4, 6};
+        periods[5] = new int[]{5};
+        periods[6] = new int[]{6};
+        periods[7] = new int[]{7, 9, 3, 1};
+        periods[8] = new int[]{8, 4, 2, 6};
+        periods[9] = new int[]{9, 1};
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            a %= 10;
+            b %= periods[a].length;
+            b = b == 0 ? periods[a].length : b;
+
+            if (a == 0) {
+                System.out.println(10);
+                continue;
+            }
+            System.out.println((int)Math.pow(a,b)%10);
+        }
+    }
+}
