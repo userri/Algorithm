75b186d2aea946f1b86f5e0447683d44dd83ddf0
Userri <125868433+userri@users.noreply.github.com>
2026-02-07 16:01:40 +0900

    [Silver II] Title: 수 이어 쓰기, Time: 288 ms, Memory: 49576 KB -BaekjoonHub

--- 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.java
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.java
@@ -1,35 +1,30 @@
 
-
 /*
 대략 숫자는 1050 근처까지
  */
 import java.lang.*;
 import java.io.*;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
 
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split("");
-        Queue<String> str = new LinkedList<>();
-        for (String s : input) {
-            str.add(s);
-        }
+        int len = input.length;
         int i = 1;
-        while (!str.isEmpty()) {
+        int pointer = 0;
+        while (pointer < len) {
             String[] iStr = (i + "").split("");
             for (String s : iStr) {
-                if (str.isEmpty()) {
+                if (pointer >= len) {
                     break;
                 }
-                if (str.peek().equals(s)) {
-                    str.poll();
+                if (input[pointer].equals(s)) {
+                    pointer++;
                 }
             }
-            if (str.isEmpty()) {
+            if (pointer >= len) {
                 System.out.println(i);
                 break;
             }
