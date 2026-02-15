88cff102271f73f96f6a2eb005a46f1398ecd024
Userri <125868433+userri@users.noreply.github.com>
2026-02-15 11:01:49 +0900

    [Bronze V] Title: 제리와 톰, Time: 124 ms, Memory: 16192 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int a = Integer.parseInt(input[0]);
+        int b = Integer.parseInt(input[1]);
+        System.out.println(b-a + " " + b);
+    }
+}
