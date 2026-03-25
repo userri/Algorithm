9476fb687f32c82998c95d901914405435e4d9d9
Userri <125868433+userri@users.noreply.github.com>
2026-03-25 13:54:12 +0900

    [level 2] Title: 피로도, Time: 3.44 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,44 +1,21 @@
 
 
-
-import java.util.Arrays;
-
 class Solution {
-    static StringBuilder sb = new StringBuilder();
-    static boolean[] visited;
-    static int[][] dungeons;
-    static int k;
-    static boolean check;
+    private int maxCnt = 0;
     public int solution(int k, int[][] dungeons) {
-        this.dungeons = dungeons;
-        this.k = k;
-        // 또 백트래킹...?
-
-        int maxCnt = 0;
-        visited = new boolean[dungeons.length];
-        for(int i = dungeons.length;i >= 1;i--) {
-            check = false;
-            dfs(dungeons.length, i, 0);
-            if(check) {
-                maxCnt = i;
-                break;
-            }
-        }
+        boolean[] visited = new boolean[dungeons.length];
+        dfs(0, k, dungeons, visited);
 
         return maxCnt;
     }
-    static void dfs(int N, int M, int depth) {
-        if(check) return;
-        if(depth == M) {
-            check = true;
-            return;
-        }
-        for(int i = 0; i< N;i++) {
-            if(!visited[i] && k >= dungeons[i][0]) {
+
+    private void dfs(int depth, int curK, int[][] dungeons, boolean[] visited) {
+        maxCnt = Math.max(maxCnt, depth);
+
+        for(int i = 0; i < dungeons.length;i++) {
+            if(!visited[i] && curK >= dungeons[i][0]) {
                 visited[i] = true;
-                k -= dungeons[i][1]; // 피로도 깎기
-                dfs(N, M, depth+1);
-                k += dungeons[i][1]; // 피로도 다시 회복
+                dfs(depth+1, curK - dungeons[i][1], dungeons, visited);
                 visited[i] = false;
             }
         }
