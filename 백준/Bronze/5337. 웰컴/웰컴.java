b13312832f5ee51d49a1d67ae3b9f8e5265cc677
Userri <125868433+userri@users.noreply.github.com>
2026-03-28 12:44:12 +0900

    [Bronze V] Title: 웰컴, Time: 96 ms, Memory: 13928 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
