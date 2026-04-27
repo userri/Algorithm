2ede0a3fdb3ba0423c312780c56e562edb8b3e44
Userri <125868433+userri@users.noreply.github.com>
2026-04-27 19:05:01 +0900

    [Bronze IV] Title: 유림이와 하람이의 두쫀쿠 대작전, Time: 104 ms, Memory: 14068 KB -BaekjoonHub

---
+++ 백준/Bronze/35277. 유림이와 하람이의 두쫀쿠 대작전/유림이와 하람이의 두쫀쿠 대작전.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(N/2000);
+    }
+}
