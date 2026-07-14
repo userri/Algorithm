f63049bf5fa9644444386c5e00e7ea9db56ad178
imyuri <dladbfl67@naver.com>
2026-07-14 21:24:45 +0900

    [D2] Title: 숫자 배열 회전

---
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        for(int test_case = 1; test_case <= T; test_case++) {
+            int N = Integer.parseInt(br.readLine());
+            int[][] matrix = new int[N][N];
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                for(int j = 0; j < N; j++) {
+                    matrix[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+            System.out.println("#" + test_case);
+
+            // 0열에서 행감소, N-1행에서 열감소, N-1열에서 행증가
+            for(int i = 0; i < N; i++) {
+                for(int j = N-1; j >= 0; j--) {
+                    System.out.print(matrix[j][i]);
+                }
+                System.out.print(" ");
+                for(int j = N-1; j >= 0; j--) {
+                    System.out.print(matrix[N-1-i][j]);
+                }
+                System.out.print(" ");
+                for(int j = 0; j < N; j++) {
+                    System.out.print(matrix[j][N-1-i]);
+                }
+                System.out.println();
+            }
+        }
+    }
+}
