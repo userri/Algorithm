61e32e802d26b8bf4b6df08ef7553b86586d7e8b
Userri <125868433+userri@users.noreply.github.com>
2026-07-22 19:12:46 +0900

    [level 3] Title: 네트워크, Time: 2.68 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,28 +1,44 @@
 
 import java.util.*;
-
 class Solution {
+    int N, answer = 0;
+    int cnt = 0;
+    boolean[] visited;
+    List<List<Integer>> graph;
     public int solution(int n, int[][] computers) {
-        int answer = 0;
-        // i == j(자기자신)일 때 무조건 연결(1)
-        boolean[] visited = new boolean[n];
-        for (int i = 0; i < n; i++) {
-            if (dfs(i, n, computers, visited)) answer++;
+        N = computers.length;
+        graph = new ArrayList<>();
+        visited = new boolean[N];
+        
+        for(int i = 0; i < N; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                if(i == j) continue;
+                // 인접리스트 추가
+                if(computers[i][j] == 1) {
+                    graph.get(i).add(j);
+                    graph.get(j).add(i);
+                }
+                
+            }
+        }
+        for(List<Integer> li: graph) {
+            System.out.println(li);
+        }
+        for(int i = 0; i < N; i++) {
+            if(dfs(i)) answer++;
         }
         return answer;
     }
-
-    boolean dfs(int st, int n, int[][] computers, boolean[] visited) {
-        System.out.println(Arrays.toString(visited));
-        if (visited[st]) return false;
-        visited[st] = true;
-        for (int i = 0; i < n; i++) {
-            if (i == st) continue;
-            // 연결되어있고 방문한 적이 없다면 dfs
-            if (computers[st][i] == 1 && !visited[i]) {
-                dfs(i, n, computers, visited);
-                visited[i] = true;
-            }
+    boolean dfs(int x) {
+        if(visited[x]) return false;
+        
+        visited[x] = true;
+        for(int i: graph.get(x)) {
+            if(visited[i]) continue;
+            dfs(i);
         }
         return true;
     }
