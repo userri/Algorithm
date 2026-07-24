c7b322faf0d4cd444d757b35f932cdc7503a886d
Userri <125868433+userri@users.noreply.github.com>
2026-07-24 16:50:15 +0900

    [D3] Title: 햄버거 다이어트, Time: 98 ms, Memory: 29,272 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,45 +1,45 @@
-// dp 풀이(냅색)
+
+// 냅색
+// dp: 결과를 얻기 위한 최소한의 정보: 조합 말고, 제한된 칼로리 안에서 최대점수
+
 import java.util.*;
 import java.io.*;
 class Solution {
-    static int[][] foods;
-    // 특정 칼로리까지 허용될 때 최대 선호도를 저장
-    static int[] dp;
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int T = Integer.parseInt(br.readLine());
-        for(int test_case = 1; test_case <= T; test_case++) {
 
+        for(int test_case = 1; test_case <= T; test_case++) {
+            // N, L
             st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
             int L = Integer.parseInt(st.nextToken());
-
-            // i번째 음식의 선호도와 칼로리를 저장
-            foods = new int[N][2];
-            for(int i = 0; i < N; i++) {
+            int[][] foods = new int[N][2]; // 0에는 점수, 1에는 칼로리
+            
+            // N개의 줄 - T, K
+            for(int i = 0; i < N ;i++) {
                 st = new StringTokenizer(br.readLine());
                 int t = Integer.parseInt(st.nextToken());
                 int k = Integer.parseInt(st.nextToken());
                 foods[i][0] = t;
                 foods[i][1] = k;
             }
-            // 칼로리 기준 초기화
-            dp = new int[L+1];
-            // 재료를 하나씩 넣어서 dp를 업데이트
+
+            // i번째 칼로리에서 최대 선호도를 저장 -> dp[L]을 정답으로 제출
+            int[] dp = new int[10001];
+
             for(int i = 0; i < N; i++) {
                 int taste = foods[i][0];
                 int cal = foods[i][1];
                 for(int w = L; w-cal >= 0; w--) {
-                    // w-cal은 w 보다 작음
-                    // w는 큰쪽에서 작은쪽으로 이동하고있기 때문에
-                    // i번째 음식이 반영되지 않았음이 확실함
-                    // 그래서 이제 현재에 i번째 음식을 넣어주고 taste를 더해줌
-                    dp[w] = Math.max(dp[w], dp[w - cal] + taste);
+                    dp[w] = Math.max(dp[w], dp[w-cal] + taste);
                 }
             }
+            System.out.println("#"+test_case+" "+dp[L]);
+
             
-            System.out.println("#" + test_case+" "+dp[L]);
+            
         }
     }
 }
