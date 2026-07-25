e5618f818e088d95729944fb9fe13898781fb420
Userri <125868433+userri@users.noreply.github.com>
2026-07-25 15:15:22 +0900

    [level 2] Title: 배달, Time: 2.88 ms, Memory: 90.7 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,72 +1,58 @@
-/* 1번 마을에 있는 음식점에서 각 마을로 음식 배달
-N개의 마을 중에서 K 시간 이하로 배달이 가능한 마을에서만 주문을 받으려고 함
-음식 주문을 받을 수 있는 마을의 개수를 return
-그러면 마을별 배달 경로가 여러개이되, 고정됨
-visited?
-다익스트라라는 유형을 봐버리긴 했는데...
-dfs로 가야겟군
-들어갈때 체크하고 나올떄 체크빼고 흠
-;;;
-dfs 하면서 각 마을까지 가는 최단 거리를 배열에 저장해놓기
-
-*/
+// 새 마음 새 뜻 새 다익스트라
+// 전형적인 다익스트라 문제!
 import java.util.*;
 class Solution {
     List<List<int[]>> graph = new ArrayList<>();
-    int[] minTimes;
-    boolean[] visited;
+    int[] dist;
     int N;
     public int solution(int N, int[][] road, int K) {
-        // 마을 별 최소 시간 저장
-        minTimes = new int[N+1];
-        Arrays.fill(minTimes, Integer.MAX_VALUE);
-        // 방문 체크
-        visited = new boolean[N+1];
-        
         this.N = N;
-        
         for(int i = 0; i <= N+1; i++) {
             graph.add(new ArrayList<>());
         }
         for(int i = 0; i < road.length; i++) {
-            int a = road[i][0];
-            int b = road[i][1];
-            int c = road[i][2];
-            graph.get(a).add(new int[]{b,c}); // 양방향 연결
-            graph.get(b).add(new int[]{a,c});
+            int start = road[i][0];
+            int end = road[i][1];
+            int cost = road[i][2];
+            graph.get(start).add(new int[]{end, cost}); // 양방향 연결
+            graph.get(end).add(new int[]{start, cost});
         }
         
-        // 1번 마을에서 출발
-        visited[1] = true;
-        dfs(1,0);
-        // System.out.println(Arrays.toString(minTimes));
+        dijkstra();
         int answer = 0;
+        // 경계주의: 0번마을제외
         for(int i = 1; i <= N; i++) {
-            if(minTimes[i] <= K) answer++;
+            if(dist[i] <= K) answer++;
         }
         return answer;
-        
     }
-    void dfs(int a, int sum) {
-        // System.out.println(a + " 방문, 합계: " + sum); 
-        // 이미 더 적은비용으로 초기화된적있으면 진행하지 않음
-        if(minTimes[a] < sum) {
-            // System.out.println(minTimes[a]+" < "+sum+" : 리턴");
-            return;
-        } else {
-            minTimes[a] = sum;
-        }
+    void dijkstra() {
+        // i번째 집까지의 최소거리를 저장
+        dist = new int[N+1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[1] = 0;
         
-        for(int[] bc: graph.get(a)) {
-            int b = bc[0];
-            int c = bc[1];
-            // 방문한적있으면 건너뛰기
-            if(visited[b]) continue;
-            visited[b] = true;
-            // 합을 업데이트해서 같이 넘겨
-            dfs(b, sum + c);
-            visited[b] = false;
+        // {도착점, 비용} 저장. 비용 오름차순으로 정렬
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);
+        pq.offer(new int[]{1,0});
+        while(!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            int node = cur[0];
+            int cost = cur[1];
+            
+            // 근데 for문안에서 어차피 초기화될텐데 굳이? 아 넣는건 여러번 넣고 뺄때 또 검사?
+            // 더 작은 값으로 초기화된 적 있으면 건너뛰기
+            if(cost > dist[node]) continue;
+            
+            for(int[] next: graph.get(node)) {
+                int nx = next[0];
+                int nc = next[1];
+                if(cost + nc < dist[nx]) {
+                    pq.offer(new int[]{nx, cost + nc});
+                    dist[nx] = cost + nc;
+                }
+            }
         }
-        
+            
     }
 }
