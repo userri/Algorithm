6ba80e4c50850314583ef290a45f6a324c5aa3c5
Userri <125868433+userri@users.noreply.github.com>
2026-08-18 11:44:12 +0900

    Time: 2 ms (21.13%), Space: 69.5 MB (11.52%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -0,0 +1,29 @@
+/**
+합이 target이상인 subarray 중에 최소길이인것
+ */
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        
+        int sum = nums[0], answer = Integer.MAX_VALUE;
+        int l = 0,r= 0;
+        while(r < nums.length) {
+            if(sum < target) {
+                if(r + 1 < nums.length) {
+                    r++;
+                    sum += nums[r];
+                } else break; // 늘려야되는데 인덱스 넘어가기 시작하면 break;
+            } else  { // sum >= target일 때
+                answer = Math.min(answer, r - l + 1);
+                if(l + 1 <= r) {
+                    sum -= nums[l];
+                    l++;
+                } else {
+                    if(r + 1 >= nums.length) break;
+                    r++;
+                    sum += nums[r];
+                }
+            }
+        }
+        return answer == Integer.MAX_VALUE ? 0 : answer;
+    }
+}
