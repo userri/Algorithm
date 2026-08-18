0b77e0959841053ec5167b08f34188e3dc5d2188
Userri <125868433+userri@users.noreply.github.com>
2026-08-18 12:05:24 +0900

    fix: l과 r 등호일때는 조건위반이 아님

--- LeetCode/0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ LeetCode/0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -6,14 +6,10 @@ class Solution {
         for(int r = 0; r < nums.length; r++) {
 
             sum += nums[r];
-            // l을 더해도 되는 상황일때만 진입해(l = r 등호도 제외)
-            while(l < r && sum >= target) {
+            while(sum >= target) {
                 answer = Math.min(answer, r - l + 1);
-                sum -= nums[l];
-                l++;
+                sum -= nums[l++];
             }
-            
-            if(sum >= target) answer = Math.min(answer, r - l + 1);
         }
         return answer == Integer.MAX_VALUE ? 0 : answer;
     }
