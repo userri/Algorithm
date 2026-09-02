a7e245626589c0e8fddc97ef9497c136071e9823
Userri <125868433+userri@users.noreply.github.com>
2026-09-02 16:14:01 +0900

    [D1] Title: 신문 헤드라인, Time: 77 ms, Memory: 24,832 KB -BaekjoonHub

---
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -0,0 +1,11 @@
+
+import java.io.*;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(br.readLine().toUpperCase());
+	}
+}
