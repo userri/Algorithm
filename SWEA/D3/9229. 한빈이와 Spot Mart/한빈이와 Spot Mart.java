97b6a5a2c5f018b828db1c65c42afa5eeae03427
Userri <125868433+userri@users.noreply.github.com>
2026-09-03 11:25:40 +0900

    [D3] Title: 한빈이와 Spot Mart, Time: 147 ms, Memory: 31,360 KB -BaekjoonHub

--- SWEA/D3/9229. 한빈이와 Spot Mart/한빈이와 Spot Mart.java
+++ SWEA/D3/9229. 한빈이와 Spot Mart/한빈이와 Spot Mart.java
@@ -1,8 +1,10 @@
-
-
 import java.util.*;
 import java.io.*;
 
+/*
+ * 최대한 많은 과자봉지. 한번에 들고다닐수있는 최대 무게 합 출력
+ * 과자 정확히 두 봉지 사야 함!
+ * */
 class Solution {
 	public static void main(String args[]) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,12 +22,21 @@ class Solution {
 			for (int i = 0; i < N; i++) {
 				weights[i] = Integer.parseInt(st.nextToken());
 			}
+			Arrays.sort(weights);
 			int result = -1;
-			for (int i = 0; i < N; i++) {
-				for (int j = i + 1; j < N; j++) {
-					int sum = weights[i] + weights[j];
-					if(sum > M) continue;
+			int l = 0, r = N - 1;
+
+			while (l < r) {
+				int sum = weights[l] + weights[r];
+				if (sum <= M) {
 					result = Math.max(result, sum);
+					if (l + 1 >= r)
+						r--;
+					else
+						l++;
+				} else {
+					// 만약 합이 초과하면 큰쪽을 줄이기
+					r--;
 				}
 			}
 
