aaeff511410e74597b3a2cae798a670739d74979
Userri <125868433+userri@users.noreply.github.com>
2026-09-09 15:41:57 +0900

    [D3] Title: N-Queen, Time: 82 ms, Memory: 25,728 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.java
+++ SWEA/D3/2806. N－Queen/N－Queen.java
@@ -1,46 +1,45 @@
 
 import java.util.*;
 import java.io.*;
+
 class Solution {
-    static int N;
-    static int cnt = 0;
-    static boolean[] cols;
-    static boolean[] upright;
-    static boolean[] downright;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        
+	static boolean[] row, col, rightUp, rightDown;
+	static int N, answer;
 
-        for(int test_case = 1; test_case <= T; test_case++) {
-            N = Integer.parseInt(br.readLine());
-            cols = new boolean[N];
-            // 대각선 범위 0~ 2(N-1);
-            downright = new boolean[2 * N - 1];
-            upright = new boolean[2 * N]; // 대각선 범위 행 - 열: 0 - (N-1) ~ N-1 - 0
-            // N을 더해서 1~2*N - 1으로 만들기
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int T = Integer.parseInt(br.readLine());
 
-            backtracking(0);
-            
-            System.out.println("#"+test_case + " "+cnt);
-            cnt = 0;
-        }
-    }
+		for (int test_case = 1; test_case <= T; test_case++) {
+			N = Integer.parseInt(br.readLine());
+			// 가로, 세로, 우상향대각선, 우하향대각선 각각 저장
+			row = new boolean[N];
+			col = new boolean[N];
+			rightUp = new boolean[2 * N - 1]; // 대각선 안에서 가로 + 세로 일정. 0+0 ~ 2N-2;
+			rightDown = new boolean[2 * N - 1]; // 가로 - 세로 일정. (0 - N+1) ~ (N-1 - 0) -> N-1씩 더해줘서 0~2*N-2로 보정
 
-    static void backtracking(int row) {
-        if(row >= N) {
-            cnt++;
-            return;
-        }
-        for(int col = 0; col < N; col++) {
-            if(cols[col] || downright[row + col] || upright[row - col + N]) continue;
-            cols[col] = true;
-            downright[row + col] = true;
-            upright[row - col + N] = true;
-            backtracking(row + 1);
-            cols[col] = false;
-            downright[row + col] = false;
-            upright[row - col + N] = false;
-        }
-    }
+			answer = 0;
+			dfs(0);
+
+			System.out.println("#" + test_case + " " + answer);
+		}
+	}
+
+	// 몇번째 row까지 검사했는지를 넘김
+	static void dfs(int rowCnt) {
+		if (rowCnt == N) {
+			answer++;
+			return;
+		}
+		row[rowCnt] = true;
+		for (int j = 0; j < N; j++) {
+			if (col[j] || rightUp[rowCnt + j] || rightDown[rowCnt - j + N - 1])
+				continue;
+			col[j] = rightUp[rowCnt + j] = rightDown[rowCnt - j + N - 1] = true;
+			dfs(rowCnt + 1);
+			col[j] = rightUp[rowCnt + j] = rightDown[rowCnt - j + N - 1] = false;
+		}
+	}
+
 }
